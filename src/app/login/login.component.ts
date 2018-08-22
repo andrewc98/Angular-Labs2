51d535223f8ae72ff29c5464d04e02a8855b3791
Andrew Campbell <andrew.campbell7@griffithuni.edu.au>
2018-08-22 17:06:38 +1000

    Local Storage Complete

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -19,6 +19,18 @@ export class LoginComponent implements OnInit {
     event.preventDefault();
     if (this.username == "Andrew" && this.password == "abc"){
       this.router.navigateByUrl('/account')
+
+      // --- Session Storage Start
+      if (typeof(Storage) !== "undefined") {
+        console.log('Storage Ready');
+        sessionStorage.setItem("user id", "1");
+        sessionStorage.setItem("username", this.username);
+        sessionStorage.setItem("dob", "January 1 2000 00:00");
+        sessionStorage.setItem("age", "18");
+        console.log(sessionStorage.getItem("dob"));
+      }
+      // --- Session Storage End
+
     } else {
       alert('No Bueno')
     }
